fdfcbc81cb3efb41aa6fe8e41c810d4bdb8ff0f8
sasabrina <sabrinaealvarez@gmail.com>
2019-10-02 15:49:46 -0300

    geocode no va mas. addMarker agrega marcadores iterando un arreglo de locaciones

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,31 +1,72 @@
+class Location {
+    constructor(name, address, phoneNumber, category, lat, lng) {
+        this.name = name,
+        this.address = address,
+        this.phoneNumber = phoneNumber,
+        this.category = category,
+        this.coords = {
+            'lat': lat, 'lng': lng
+        }
+    }   
+}
+
+
+//Variables globales
 const mapNode = document.getElementById('map');
 let map, marker, infoWindow;
-let coord = {lat: -34.6131500, lng: -58.3772300}
+let intialCoords = {lat: -34.6131500, lng: -58.3772300}
 
-let infoContent = `<p>Descripción: Aeroterra S.A.</p>
-<p>Dirección: Av. Eduardo Madero 1020</p>
-<p>Teléfono: 459 11 5272 0900</p>
-<p>(X, Y): -34.5972551, -58.3722446</p>
-<p>Categoría: Residencial</p>`
+//harcodeo datos para probar funciones
+const locations = [
+    {
+        name: 'Aeroterra', 
+        address: 'Av. E. Madero 1020', 
+        phoneNumber: '549 11 5272 0900', 
+        category: 'Residencial', 
+        coords: {
+            lat: -34.595986,
+            lng: -58.3724715
+        }
+    },
+    {
+        name: 'Workinn', 
+        address: 'Pres. Tte. Gral. Juan Domingo Perón 698', 
+        phoneNumber: '3333333', 
+        category: 'Residencial', 
+        coords: {
+            lat: -34.6058542,
+            lng: -58.3786129
+        }
+    }
+]
 
+//funciones auxiliares
+//función que trae los valores del formulario
+const getInputValue = (id) => {
+    return document.getElementById(id).value
+}
+
+//función que carga el mapa
 function initMap (){
     map = new google.maps.Map(mapNode, {
-        center: coord,
+        center: intialCoords,
         zoom: 12
     });
 
-    addMarker()
+    locations.map(l => addMarker(l))
     
 }
 
-const addMarker = () => {
+
+//función que agrega el marcador en el mapa
+const addMarker = location => {
     marker = new google.maps.Marker({
-        position: {lat: -34.5972551, lng: -58.3722446},
+        position: location.coords,
         map: map
     })
 
     infoWindow = new google.maps.InfoWindow({
-        content: infoContent
+        content: markerPoup(location)
     })
 
     marker.addListener('click', function(){
@@ -33,17 +74,31 @@ const addMarker = () => {
     })
 }
 
-//funcion para obtener coordenadas
-const getCoords = placeName =>{
-    let encondedPlaceName = encodeURIComponent(placeName)
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encondedPlaceName}&key=AIzaSyDriPWRjCuM0jQ6d6oTPoGaWo2EU_x3nJU`)
-    .then(response => response.json())
-    .then(res => {
-        let { geometry:{ location } } = res.results[0]
-        console.log(location);
-        
-    })
-    .catch(error => console.log(error))
-}
 
-getCoords('Av. E. Madero 1020')
+const getFormData = () => {
+    event.preventDefault()
+    let name = getInputValue('form-name'),
+        address = getInputValue('form-address'),
+        phoneNumber = getInputValue('form-phone'),
+        category = getInputValue('form-category'),
+        lat = parseFloat(getInputValue('form-coord-lat')),
+        lng = parseFloat(getInputValue('form-coord-lng'))
+
+
+    let newLocation = new Location(name, address, phoneNumber, category, lat, lng)
+    console.log(newLocation);
+    locations.push(newLocation)
+    console.log(locations);
+    
+    
+}
+//crea el contenido de infoWindow
+const markerPoup = data => {
+    let {name, address, phoneNumber, category, coords} = data
+
+    return `<p>Descripción: ${name}.</p>
+    <p>Dirección: ${address}</p>
+    <p>Teléfono: ${phoneNumber}</p>
+    <p>(X, Y): ${coords.lat}, ${coords.lng}</p>
+    <p>Categoría: ${category}</p>`
+}
