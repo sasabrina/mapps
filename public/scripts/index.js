95778ce8c7cc745ab668c246cce3e8996adb7a7a
sasabrina <sabrinaealvarez@gmail.com>
2019-10-02 00:49:59 -0300

    addMarker: agrega marcador harcodeado

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,12 @@
 const mapNode = document.getElementById('map');
-let map;
+let map, marker, infoWindow;
 let coord = {lat: -34.6131500, lng: -58.3772300}
+
+let infoContent = `<p>Descripción: Aeroterra S.A.</p>
+<p>Dirección: Av. Eduardo Madero 1020</p>
+<p>Teléfono: 459 11 5272 0900</p>
+<p>(X, Y): -34.5972551, -58.3722446</p>
+<p>Categoría: Residencial</p>`
 
 function initMap (){
     map = new google.maps.Map(mapNode, {
@@ -8,4 +14,21 @@ function initMap (){
         zoom: 12
     });
 
+    addMarker()
+    
+}
+
+const addMarker = () => {
+    marker = new google.maps.Marker({
+        position: {lat: -34.5972551, lng: -58.3722446},
+        map: map
+    })
+
+    infoWindow = new google.maps.InfoWindow({
+        content: infoContent
+    })
+
+    marker.addListener('click', function(){
+        infoWindow.open(map, marker)
+    })
 }
