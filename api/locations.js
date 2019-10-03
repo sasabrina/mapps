44d4b946597a468c613f44399f95b27ca5b226c4
sasabrina <sabrinaealvarez@gmail.com>
2019-10-03 17:45:56 -0300

    fix api key

--- api/locations.js
+++ api/locations.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 
-let locations = fs.readFileSync('locations.json')
+let locations = fs.readFileSync('tmp/locations.json')
 locations = JSON.parse(locations)
 
-const getLocation = (req, res, next) => {
+const getLocation = (req, res) => {
     res.json( {locations} )
 }
 
-const postLocation = (req, res, next) => {
+const postLocation = (req, res) => {
     let data = req.body
     
     if(data.hasOwnProperty('coords')){
         locations.push(data)
-        fs.writeFileSync('locations.json', JSON.stringify(locations))
+        fs.writeFileSync('tmp/locations.json', JSON.stringify(locations))
         res.status('200').json(`Location received`)
     } else {
         res.status('400').json('Opps! Somenthing went wrong')
