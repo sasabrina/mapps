b66d0806a90d5284b22bdc91683def3bd900b398
sasabrina <sabrinaealvarez@gmail.com>
2019-10-03 19:12:45 -0300

    update

--- api/locations.js
+++ api/locations.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-let locations = fs.readFileSync('tmp/locations.json')
+let locations = fs.readFileSync('locations.json')
 locations = JSON.parse(locations)
 
 const getLocation = (req, res) => {
@@ -12,7 +12,7 @@ const postLocation = (req, res) => {
     
     if(data.hasOwnProperty('coords')){
         locations.push(data)
-        fs.writeFileSync('tmp/locations.json', JSON.stringify(locations))
+        fs.writeFileSync('locations.json', JSON.stringify(locations))
         res.status('200').json(`Location received`)
     } else {
         res.status('400').json('Opps! Somenthing went wrong')
